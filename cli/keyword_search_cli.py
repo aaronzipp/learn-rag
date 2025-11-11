57a518748480105ebda093314a46daf7a0174f55
Aaron Zipp <mail@aaronzipp.de>
2025-11-11 15:48:22 +0100

    Remove punctuation

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -1,4 +1,21 @@
 import argparse
+import json
+import re
+import string
+
+PUNCTUATION_TABLE = str.maketrans(string.punctuation, " " * len(string.punctuation))
+
+
+def remove_punctuation(input_text: str):
+    return input_text.translate(PUNCTUATION_TABLE)
+
+
+def remove_multiple_whitespace(input_text: str):
+    return re.sub(r"\s{2,}", " ", input_text.strip())
+
+
+def process_text(input_text: str):
+    return remove_multiple_whitespace(remove_punctuation(input_text.lower()))
 
 
 def main() -> None:
@@ -10,9 +27,18 @@ def main() -> None:
 
     args = parser.parse_args()
 
+    with open("movies.json", "r") as f:
+        movies = json.load(f)
+
     match args.command:
         case "search":
             print(f"Searching for: {args.query}")
+            search_term = process_text(args.query)
+            n_results = 1
+            for movie in movies["movies"]:
+                if search_term in process_text(movie["title"]):
+                    print(f"{n_results}. {movie['title']}")
+                    n_results += 1
         case _:
             parser.print_help()
 
