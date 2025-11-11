9d1e93084c0344a8b97a54e47a7534e64e421542
Aaron Zipp <mail@aaronzipp.de>
2025-11-11 21:23:02 +0100

    Use typer for cli

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-import argparse
 import math
+
+import typer
 
 from hoopla.index import InvertedIndex, load_index
 from hoopla.processing import generate_tokens, get_single_token
 
+app = typer.Typer()
 
+
+@app.command()
 def search(search_term: str) -> None:
+    print(f"Searching for: {search_term}")
     index = load_index()
 
     search_tokens = generate_tokens(search_term)
@@ -19,62 +24,38 @@ def search(search_term: str) -> None:
         print(f"{i + 1}. {index.docmap[result]}")
 
 
-def main() -> None:
-    parser = argparse.ArgumentParser(description="Keyword Search CLI")
-    subparsers = parser.add_subparsers(dest="command", help="Available commands")
+@app.command()
+def build() -> None:
+    index = InvertedIndex()
+    index.build()
+    index.save()
 
-    search_parser = subparsers.add_parser("search", help="Search movies using BM25")
-    search_parser.add_argument("query", type=str, help="Search query")
 
-    subparsers.add_parser("build", help="Build the inverted index")
+@app.command()
+def tf(doc_id: int, term: str) -> None:
+    index = load_index()
+    print(index.get_tf(doc_id, term))
 
-    tf_parser = subparsers.add_parser(
-        "tf", help="Get the term frequency of a term for a document"
-    )
-    tf_parser.add_argument("doc_id", type=int, help="Document ID")
-    tf_parser.add_argument("term", type=str, help="A term")
 
-    idf_parser = subparsers.add_parser(
-        "idf", help="Get the inverse document frequency of a term"
-    )
-    idf_parser.add_argument("term", type=str, help="A term")
+@app.command()
+def idf(term: str):
+    index = load_index()
+    token = get_single_token(term)
+    term_doc_count = len(index.index[token])
+    idf = math.log((len(index.docmap) + 1) / (term_doc_count + 1))
+    print(f"Inverse document frequency of '{term}': {idf:.2f}")
 
-    tfidf_parser = subparsers.add_parser("tfidf", help="Get the tfidf of a term")
-    tfidf_parser.add_argument("doc_id", type=int, help="Document ID")
-    tfidf_parser.add_argument("term", type=str, help="A term")
 
-    args = parser.parse_args()
-
-    match args.command:
-        case "search":
-            print(f"Searching for: {args.query}")
-            search(args.query)
-        case "build":
-            index = InvertedIndex()
-            index.build()
-            index.save()
-        case "tf":
-            index = load_index()
-            print(index.get_tf(args.doc_id, args.term))
-        case "idf":
-            index = load_index()
-            token = get_single_token(args.term)
-            term_doc_count = len(index.index[token])
-            idf = math.log((len(index.docmap) + 1) / (term_doc_count + 1))
-            print(f"Inverse document frequency of '{args.term}': {idf:.2f}")
-        case "tfidf":
-            index = load_index()
-            token = get_single_token(args.term)
-            tf = index.get_tf(args.doc_id, args.term)
-            term_doc_count = len(index.index[token])
-            idf = math.log((len(index.docmap) + 1) / (term_doc_count + 1))
-            tf_idf = tf * idf
-            print(
-                f"TF-IDF score of '{args.term}' in document '{args.doc_id}': {tf_idf:.2f}"
-            )
-        case _:
-            parser.print_help()
+@app.command()
+def tfidf(doc_id: int, term: str) -> None:
+    index = load_index()
+    token = get_single_token(term)
+    tf = index.get_tf(doc_id, term)
+    term_doc_count = len(index.index[token])
+    idf = math.log((len(index.docmap) + 1) / (term_doc_count + 1))
+    tf_idf = tf * idf
+    print(f"TF-IDF score of '{term}' in document '{doc_id}': {tf_idf:.2f}")
 
 
 if __name__ == "__main__":
-    main()
+    app()
