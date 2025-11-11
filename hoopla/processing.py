c1f51f0e3b98512b90452b5269b26d7b97c93a01
Aaron Zipp <mail@aaronzipp.de>
2025-11-11 16:44:11 +0100

    Restructured project

---
+++ hoopla/processing.py
@@ -0,0 +1,19 @@
+import string
+
+from nltk.stem import PorterStemmer
+
+PUNCTUATION_TABLE = str.maketrans(string.punctuation, " " * len(string.punctuation))
+
+with open("data/stopwords.txt") as f:
+    STOPWORDS = f.read().splitlines()
+
+STEMMER = PorterStemmer()
+
+
+def remove_punctuation(input_text: str):
+    return input_text.translate(PUNCTUATION_TABLE)
+
+
+def generate_tokens(input_text: str) -> list[str]:
+    tokens = remove_punctuation(input_text.lower()).split()
+    return [STEMMER.stem(token) for token in set(tokens) - set(STOPWORDS)]
